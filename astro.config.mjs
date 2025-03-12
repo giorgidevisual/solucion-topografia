1219d43e411112ee710ea765ff165408e88ac0e8
Giorgi Visual <giorgivisual@gmail.com>
2025-03-12 20:56:01 +0100

    create ButtonNavigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
+import node from '@astrojs/node';
+import htmx from 'astro-htmx';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [htmx()],
   output: "server",
   adapter: node({
     mode: "middleware",
